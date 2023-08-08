51907132dbf8421cd574e8d431009dcc06e61495
aj-marci <abmarcinek@gmail.com>
2023-08-08 13:34:47 -0500

    finished oauth access and refresh tokens for server, connected react app to server tokens, and started using concurrently to npm start client and server

--- index.js
+++ index.js
@@ -72,26 +72,48 @@ app.get('/callback', (req, res) => {
     .then(response => {
         if (response.status === 200) {
 
-          const { access_token, token_type } = response.data;
+          const { access_token, refresh_token } = response.data;
 
-          axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-              Authorization: `${token_type} ${access_token}`
-            }
+          const queryParams = querystring.stringify({
+            access_token,
+            refresh_token,
           })
-          .then(response => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch(error => {
-            res.send(error);
-          });
+
+          // redirect to react app
+          res.redirect(`http://localhost:3000/?${queryParams}`)
+
+          // pass tokens in query params
         } else {
-            res.send(response);
+            res.redirect(`/?${querystring.stringify({ error:
+            'invalid token' })}`);
           }
         })
         .catch(error => {
           res.send(error);
         });
+  });
+
+  app.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
   });
 
 app.listen(port, () => {
