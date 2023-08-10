d8fac6bcb2ca43157d08425d85682c188a0b4697
aj-marci <abmarcinek@gmail.com>
2023-08-10 13:05:44 -0500

    began fetching spotify json data to front end. added axios global request headers, added profile data state, and put and data loading errors in HOF (utils.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { accessToken, logout } from './spotify';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 
 function App() {
 
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
 useEffect(() => {
   setToken(accessToken);
+
+const fetchData = async () => {
+    const { data } = await getCurrentUserProfile();
+    setProfile(data);
+}
+
+catchErrors(fetchData());
+
 },[])
 
   return (
@@ -25,6 +35,17 @@ useEffect(() => {
         <>
         <h1>Logged in!</h1>
         <button onClick={logout}>Logout</button>
+
+          {profile && (
+            <div>
+            <h1>{profile.display_name}</h1>
+            <p>{profile.followers.total} Followers</p>
+            {profile.images.length && profile.images[0].url && (
+              <img src={profile.images[0].url} alt="Avatar"></img>
+            )}
+            </div>
+          )}
+
         </>
         )}
       </header>
